8e285dc85cc2936cb660aa21dd191f3d60ed8129
Fahim Arzu <fahimewcse@gamil.com>
2026-02-06 22:15:00 +0600

    added signup

--- Day-13/formApp/src/app/app.routes.ts
+++ Day-13/formApp/src/app/app.routes.ts
@@ -7,10 +7,12 @@ export const routes: Routes = [
     {path: '', component:Home},
 
     {path:'contact', loadComponent: () => import('./contact/contact').then(m=>m.Contact)},
+    {path:'signup', loadComponent: ()=>import('./signup/signup').then(m=>m.Signup)},
     {path:'login', loadComponent: () => import('./login/login').then(m=>m.Login)},
     {path:'dashboard', loadComponent: () => import('./dashboard/dashboard').then(m=>m.Dashboard),
         canActivate:[authGuard]
     },
+
     
 
 ];
