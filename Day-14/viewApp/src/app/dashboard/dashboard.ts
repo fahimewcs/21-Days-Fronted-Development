770b2c65152e6d28f5043f100a3aff04f1cf564a
Fahim Arzu <fahimewcse@gamil.com>
2026-02-08 07:33:15 +0600

    API data showing

--- Day-14/viewApp/src/app/dashboard/dashboard.ts
+++ Day-14/viewApp/src/app/dashboard/dashboard.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { Auth } from '../authenticate/auth';
 import { CommonModule } from '@angular/common';
+import { User } from './user';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,10 +13,19 @@ import { CommonModule } from '@angular/common';
 export class Dashboard {
 
   constructor(
-    public auth: Auth, private router: Router) {}
+    public auth: Auth, private router: Router, private userService:User) {}
 
   logout() {
     this.auth.logout();
     this.router.navigate(['/login']);
+  }
+
+  users: any []=[];
+
+  ngOnInit(){
+    this.userService.getUsers().subscribe((data:any)=>{
+      this.users = data;
+      console.log(this.users)
+    })
   }
 }
